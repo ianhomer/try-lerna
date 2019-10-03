15854c1e9e626a483fb658ed753a4df11a37c620
Ian Homer <ian.homer@gmail.com>
2019-10-03 06:59:01 +0100

    React rewired

---
+++ services/with-typescript/with-typescript-ui/config-overrides.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const {
+  babelInclude,
+  override,
+  removeModuleScopePlugin
+} = require("customize-cra");
+
+module.exports =  override(
+  babelInclude([
+    path.resolve("src"), // make sure you link your own source
+    fs.realpathSync("node_modules/@try-lerna/typescript-package"),
+    fs.realpathSync("node_modules/@try-lerna/typescript-react-package")
+  ]),
+  removeModuleScopePlugin()
+)
